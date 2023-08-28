36015b1ea3f2cdcaac3fd977931fd01924460859
Greck2908 <rallistab-82@bk.ru>
2023-08-29 02:31:35 +0600

    update: index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    n = Math.abs(n);
+    parse = parseInt(n.toString().split('').reverse().join('')) * Math.sign(n);
+    return (parse);
 }
